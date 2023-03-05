b0c87faa649930735e049a98c316973e8e38a072
Tomax47 <tammam744@gmail.com>
2023-03-05 19:59:33 +0300

    New commit

--- src/Exception/Main.java
+++ src/Exception/Main.java
@@ -8,64 +8,128 @@ import java.util.Scanner;
 public class Main extends Exception{
     public static void main(String[] args) throws Exceptions {
 
+        //DIVIDING BY ZERO EXCEPTION
+
         Scanner scan = new Scanner(System.in);
         Random rnd = new Random();
 
+//        try{
+//            int a = rnd.nextInt(-50, 50);
+//            System.out.println("a = " + a);
+//            System.out.print("x = ");
+//            double x = scan.nextDouble();
+//            double f = F(x,a);
+//            if (x != a) {
+//                System.out.println("F(x) = "+f);
+//            } else if (x == a) {
+//                throw new Exceptions("Numerical error!",x,a);
+//            }
+//        } catch (Exceptions e) {
+//            System.out.println(e.getMessage());
+//        } catch (InputMismatchException e) {
+//            System.out.println("You didn't enter a number!");
+//        } finally {
+//            System.out.println("All good!");
+//        }
+//
+//        System.out.println("\n===================================================\n");
+
+        //OUTTA OF BOUND EXCEPTION
+//        try{
+//            int[] array = new int[10];
+//            for (int i = 0; i < array.length; i++) {
+//                array[i] = rnd.nextInt(-10,10);
+//            }
+//            System.out.println(Arrays.toString(array));
+//            System.out.println("\n");
+//
+//            System.out.print("Enter a starting bound : ");
+//            int startingBound = scan.nextInt();
+//            System.out.print("Enter an ending bound : ");
+//            int endingBound = scan.nextInt();
+//
+//            if (endingBound<= array.length) {
+//                for (int i = startingBound; i <endingBound ; i++) {
+//                    array[i] = rnd.nextInt(-10,10);
+//                }
+//                System.out.println(Arrays.toString(array));
+//            } else if (endingBound> array.length) {
+//                throw new OutOfBound("",array,endingBound);
+//            }
+//        } catch (OutOfBound e) {
+//            System.out.println(e.getMessage());
+//        }
+
+
+        //THIRD TASK
+
+        //#1
+//        try {
+//            int a,b,n;
+//            System.out.print("Enter a : ");
+//            a = scan.nextInt();
+//            System.out.print("Enter b : ");
+//            b = scan.nextInt();
+//            System.out.print("Enter the size of the array  : ");
+//            n = scan.nextInt();
+//            int[] array2 = new int[n];
+//            if (a<= array2.length && b<= array2.length) {
+//                for (int i = 0; i < array2.length; i++) {
+//                    if (i>=a && i<=b) {
+//                        System.out.print("Enter an integer : ");
+//                        array2[i] = scan.nextInt();
+//                    } else {
+//                        array2[i] = rnd.nextInt(-10,10);
+//                    }
+//                }
+//                System.out.println("\n");
+//
+//                for (int i = 0; i < array2.length; i++) {
+//                    System.out.print(array2[i]+" ");
+//                }
+//            } else if (a> array2.length | b> array2.length) {
+//                throw new OutOfBound(" ",array2,a,b);
+//            }
+//        } catch (OutOfBound e) {
+//            System.out.println(e.getMessage());
+//        }
+
+        //#2
         try{
-            int a = rnd.nextInt(-50, 50);
-            System.out.println("a = " + a);
-            System.out.print("x = ");
-            double x = scan.nextDouble();
-            double f = F(x,a);
-            if (x != a) {
-                System.out.println("F(x) = "+f);
-            } else if (x == a) {
-                throw new Exceptions("Numerical error!",x,a);
+            double x; int a;
+            double[] fArray = new double[5];
+            for (int i = 0; i < fArray.length; i++) {
+                System.out.print("Enter x : ");
+                x = scan.nextDouble();
+                System.out.print("Enter a : ");
+                a = scan.nextInt();
+                if (x != a){
+                    fArray[i] = F(x,a);
+                } else {
+                    throw new Exceptions("Numerical error!",x,a);
+                }
+
+
+            }
+            for (int i = 0; i < fArray.length; i++) {
+                System.out.print(fArray[i]+" , ");
             }
         } catch (Exceptions e) {
             System.out.println(e.getMessage());
         } catch (InputMismatchException e) {
             System.out.println("You didn't enter a number!");
-        } finally {
-            System.out.println("All good!");
-        }
-
-        System.out.println("\n===================================================\n");
-        try{
-            int[] array = new int[10];
-            for (int i = 0; i < array.length; i++) {
-                array[i] = rnd.nextInt(-10,10);
-            }
-            System.out.println(Arrays.toString(array));
-            System.out.println("\n");
-
-            System.out.print("Enter a starting bound : ");
-            int startingBound = scan.nextInt();
-            System.out.print("Enter an ending bound : ");
-            int endingBound = scan.nextInt();
-
-            if (endingBound<= array.length) {
-                for (int i = startingBound; i <endingBound ; i++) {
-                    array[i] = rnd.nextInt(-10,10);
-                }
-                System.out.println(Arrays.toString(array));
-            } else if (endingBound> array.length) {
-                throw new OutOfBound("",array,endingBound);
-            }
-        } catch (OutOfBound e) {
-            System.out.println(e.getMessage());
         }
 
     }
 
 
+
+    //CALCULATING THE VALUE OF THE FUNCTION
     public static double F(double x, double a) {
         if (x<0) {
             return x +Math.pow(Math.sin((1/(x-a))+4),2);
-        } else if (x>=0) {
-            return a*x/(Math.sqrt(a*a - x*x));
         } else {
-            return Double.parseDouble(null);
+            return (a*x)/(Math.sqrt(a*a - x*x));
         }
     }
 }
--- src/Exception/OutOfBound.java
+++ src/Exception/OutOfBound.java
@@ -3,6 +3,7 @@ package Exception;
 public class OutOfBound extends Exception{
     private int[] array;
     private int i;
+    private int a,b;
 
     public OutOfBound(){
         super();
@@ -12,15 +13,31 @@ public class OutOfBound extends Exception{
         super(message);
     }
 
-    public OutOfBound(String message, int[] array, int i) {
-        super(message);
-        this.array = array;
-        this.i = i;
-    }
+//    public OutOfBound(String message, int[] array, int i) {
+//        super(message);
+//        this.array = array;
+//        this.i = i;
+//    }
+
+//    public String getMessage(){
+//        String message = " ";
+//        if (i > array.length) message = "Out of bound, no storing space is left in the array!";
+//        return message;
+//    }
 
     public String getMessage(){
         String message = " ";
-        if (i > array.length) message = "Out of bound, no storing space is left in the array!";
+        if (a> array.length | b > array.length) message = "Out of bound, no storing space is left in the array!";
         return message;
     }
+
+
+    //OUT OF BOUND ERROR FOR THE THIRD TASK
+    public OutOfBound(String message, int[] array, int a, int b) {
+        super(message);
+        this.array = array;
+        this.a = a;
+        this.b = b;
+    }
+
 }
